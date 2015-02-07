598a93729b909769e13bc2fa9da10b4b148d3cc7
maxyel <max.yelsky@gmail.com>
2015-02-07 12:46:27 -0500

    "Buttons added, connection between Buttons and map"

--- app/src/main/java/com/dash/myapplication/MainScreen.java
+++ app/src/main/java/com/dash/myapplication/MainScreen.java
@@ -1,9 +1,11 @@
 package com.dash.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainScreen extends ActionBarActivity {
@@ -35,5 +37,10 @@ public class MainScreen extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void mapTime(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
