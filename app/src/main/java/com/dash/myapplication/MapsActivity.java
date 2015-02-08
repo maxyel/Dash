3823de0c238c818caddfa47eab06be0e427aee02
maxyel <max.yelsky@gmail.com>
2015-02-08 05:34:56 -0500

    Button disappears after press

--- app/src/main/java/com/dash/myapplication/MapsActivity.java
+++ app/src/main/java/com/dash/myapplication/MapsActivity.java
@@ -9,6 +9,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -271,7 +272,7 @@ public class MapsActivity extends FragmentActivity implements
 
             //mMarkers.put
         }
-
+        ((Button) view).setVisibility(View.GONE);
     }
 
     @Override
